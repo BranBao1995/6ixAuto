e68ed37ff9315cf62f035f3d660ccba115ffa2c9
BranBao1995 <brandon.0702@hotmail.com>
2022-11-24 11:10:33 -0500

    added pages and components for front-end

---
+++ client/src/pages/DreamList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+import ListingCard from "../components/ListingCard";
+// import {  } from "../utils/queries";
+// import {  } from "../utils/mutations";
+
+const DreamList = () => {
+  return <main></main>;
+};
+
+export default DreamList;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+
+import ListingCard from "../components/ListingCard";
+
+// import {  } from "../utils/queries";
+// import {  } from "../utils/mutations";
+
+const Home = () => {
+  return <main></main>;
+};
+
+export default Home;
