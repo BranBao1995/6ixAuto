334a1c82e2eea29a22aa72f23f837ab7a1f0fd4a
BranBao1995 <brandon.0702@hotmail.com>
2022-11-24 12:05:51 -0500

    added client queries and mutations, not done yet.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,6 +6,109 @@ export const GET_ME = gql`
       _id
       username
       email
+      phone
+      listings {
+        _id
+        make
+        model
+        year
+        carType
+        location
+        price
+        mileage
+        transmission
+        image
+        description
+        createdAt
+        updatedAt
+        user
+        liked
+        likeCount
+      }
+      interested {
+        _id
+        make
+        model
+        year
+        carType
+        location
+        price
+        mileage
+        transmission
+        image
+        description
+        createdAt
+        updatedAt
+        user
+        liked
+        likeCount
+      }
+    }
+  }
+`;
+
+export const GET_POST = gql`
+  query getPost {
+    getPost {
+      _id
+      make
+      model
+      year
+      carType
+      location
+      price
+      mileage
+      transmission
+      image
+      description
+      createdAt
+      updatedAt
+      user {
+        _id
+        username
+        email
+        phone
+      }
+      liked {
+        _id
+        username
+        email
+        phone
+      }
+      likeCount
+    }
+  }
+`;
+
+export const GET_SEARCH = gql`
+  query searchResults {
+    searchResults {
+      _id
+      make
+      model
+      year
+      carType
+      location
+      price
+      mileage
+      transmission
+      image
+      description
+      createdAt
+      updatedAt
+      user {
+        _id
+        username
+        email
+        phone
+      }
+      liked {
+        _id
+        username
+        email
+        phone
+      }
+      likeCount
     }
   }
 `;
