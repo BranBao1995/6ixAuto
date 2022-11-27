b0fe89d19b6849a9a9b7d62f218222ccf93d507c
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-11-27 10:10:29 -0500

    added video header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,7 +6,8 @@ import ListingCard from "../components/ListingCard";
 import MakeCard from "../components/MakeCard";
 import ModelCard from "../components/ModelCard";
 import SearchResults from "../components/SearchResult";
-
+import videoBg from '../assets/images/6ixAutoBg.mp4'
+import '../index.css'
 // import {  } from "../utils/queries";
 // import {  } from "../utils/mutations";
 
@@ -39,41 +40,59 @@ const Home = () => {
   return (
     <>
       <div className="">
-        <div className="mt-2 p-5 bg-primary text-white rounded d-flex flex-column">
-          <h1>Welcome to 6ixAuto! 'insert welcome user text here' </h1>
-          <p>Search for your dream car!</p>
+        <div className="bg-video">
+          <div className="overlay"></div>
+          <video src={videoBg} autoPlay loop muted></video>
+          <div className="content">
+            <h1>Welcome to 6ixAuto!</h1>
+            <h5>Search for your dream car!</h5>
+          </div>
         </div>
-        <div className="mt-5 d-flex flex-column justify-content-center p-2 bg-secondary">
+        <div className="mt-5 d-flex flex-column justify-content-center p-2 bg-secondary container">
           <h3> Select Your Make!</h3>
-          <Alert
-            dismissible
-            onClose={() => setShowAlert(false)}
-            show={showAlert}
-            variant="danger"
-          >
-            invalid search entry
-          </Alert>
+
           {/* create a state to decide which one to render */}
           {searchMode ? (
-            <div>
-              <MakeCard onSelect={setMakeHandler} />
-              {selections.make ? (
-                <ModelCard onSelect={setModelHandler} make={selections.make} />
-              ) : (
-                <p> Select Your Model! </p>
-              )}
-              <button
-                onClick={() => {
-                  if (selections.model) {
-                    setSearchMode(false);
-                  } else {
-                    setShowAlert(true);
-                  }
-                }}
+            <>
+              <div>
+                <MakeCard onSelect={setMakeHandler} />
+                {selections.make ? (
+                  <ModelCard
+                    onSelect={setModelHandler}
+                    make={selections.make}
+                  />
+                ) : (
+                  <p> Select Your Model! </p>
+                )}
+                <button
+                  onClick={() => {
+                    if (selections.model) {
+                      setSearchMode(false);
+                    } else {
+                      setShowAlert(true);
+                    }
+                  }}
+                >
+                  Submit Search
+                </button>
+              </div>
+              <Alert
+                className="d-flex justify-content-between"
+                onClose={() => setShowAlert(false)}
+                show={showAlert}
+                variant="danger"
               >
-                Submit Search
-              </button>
-            </div>
+                <h5>Invalid Search Entry</h5>
+                <div className="">
+                  <Button
+                    onClick={() => setShowAlert(false)}
+                    variant="outline-secondary"
+                  >
+                    Close
+                  </Button>
+                </div>
+              </Alert>
+            </>
           ) : (
             <p> </p>
           )}
