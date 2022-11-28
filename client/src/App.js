d6f471295f070194a2acc686ce76e5abff6eeea4
keyshawn bhagwandin <keyshawn.bhag@hotmail.com>
2022-11-28 11:44:59 -0500

    added touched up styling

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ function App() {
       <Router>
         <div className="entire-page">
           <Nav />
-          <div className="">
+          <div className="d-flex flex-column">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/dreamlist" element={<DreamList />} />
